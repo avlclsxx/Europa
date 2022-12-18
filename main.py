215e617d767e48f69d17e4dfe61e8566997ebe88
Aviellyan <91724903+avlclsxx@users.noreply.github.com>
2022-12-18 15:26:47 +0000

    Removed classes because the program can't run properly with it.

--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 import os
 import discord
 import openai
-
 openai.api_key = os.environ['openai_token']
 intents = discord.Intents.default()
 intents.message_content = True
+client = discord.Client(intents=intents)
 
-class Europa(discord.Client):
-    async def on_ready(self):
-        print(f'hello world! {self.user.name} are online.')
+@client.event
+async def on_ready():
+    print(f'hello world! {client.user} are online.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    print(f'{message.author}: {message.content}')
+      
+    if message.content.startswith('ping'):
+        await message.channel.send("pong! i'm here :)")
+
+    if message.content.startswith('draw'):
+        try:
+            response = openai.Image.create(
+            prompt=message.content[7:],
+            n=1,
+            size="1024x1024"
+            )     
+            image_url = response['data'][0]['url']
     
-    async def on_message(self, message):
-        if message.author == self.user:
-            return
-        print(f'{message.author}: {message.content}')
+            await message.channel.send(image_url)     
+        except Exception as e:
+            await message.channel.send('error :(')
 
-class Chatting(Europa):
-    async def on_message(self, message):
-        if message.content.startswith('ping'):
-            await message.channel.send("pong! i'm here :)")
-
-class DallE(Europa):
-    async def on_message(self, message):
-        if message.content.startswith('draw'):
-            try:
-                response = openai.Image.create(
-                    prompt=message.content[7:],
-                    n=1,
-                    size="1024x1024"
-                )     
-                image_url = response['data'][0]['url']
-    
-                await message.channel.send(image_url)     
-            except Exception as e:
-                await message.channel.send('error :(')
-
-client = Europa(intents=intents)
 client.run(os.environ['europa_token'])
